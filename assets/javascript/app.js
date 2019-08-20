3ef09df2c3f99ca6dc9226e7b1873967ad8c398d
Guillermo Villalta <guillermovillalta@Guillermos-iMac.local>
2019-08-19 20:08:39 -0600

    added logic to render all values in array when ever the submit button is hit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,26 @@
-var recepients = []
+var recipients = []
 
 $('#input-recepient').click(function(){
-    event.preventDefault();
 
-    recepients.push($("#recipient-holder").val());
-    console.log(recepients);
+    event.preventDefault();
+    $("tbody").empty();
+    
+    recipientHolder = $("#recipient-holder").val();
+    // console.log(recipientHolder);
+    
+    recipients.push(recipientHolder);
+    // console.log(recipients);
+
     $("#recipient-holder").val("")
+
+    for (var i = 0; i < recipients.length; i++) {
+        newRow = $("<td>")
+        newRow.text(recipients[i]);
+        
+        newEntry = $("<tr>");
+        newEntry.append(newRow);
+
+        $("tbody").append(newEntry);
+    }
+
 })
