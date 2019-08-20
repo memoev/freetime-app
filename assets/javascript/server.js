9cdfc2974c05e08e6335b2416e0ade2a3d5517db
codybender <codyfbender@gmail.com>
2019-08-19 19:39:30 -0600

    created data structure

---
+++ assets/javascript/server.js
@@ -0,0 +1,15 @@
+/*
+DATA STRUCTURE
+
+plans (collection)
+    auto-ID (document)
+        organizer {name: (string), email: (string)}
+        title (string)
+        urlHash (string)
+        numRecipients (int)
+        responses (collection)
+            responseN (document)
+                name (string)
+                availableTimes : {Sun : [8-10, 2-3], Sat: ...}
+*/
+
