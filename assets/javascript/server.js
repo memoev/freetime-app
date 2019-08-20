b066d65f6e4c4a50b985ffc68efb4a21c031c049
codybender <codyfbender@gmail.com>
2019-08-19 19:58:03 -0600

    added firebase

--- assets/javascript/server.js
+++ assets/javascript/server.js
@@ -1,3 +1,18 @@
+var firebaseConfig = {
+	apiKey: "AIzaSyCmpucAhOQ8Q5RSA44F6MotgDXlSRepgYY",
+	authDomain: "freetime-13631.firebaseapp.com",
+	databaseURL: "https://freetime-13631.firebaseio.com",
+	projectId: "freetime-13631",
+	storageBucket: "",
+	messagingSenderId: "731645477307",
+	appId: "1:731645477307:web:7f20c7956bdc8725"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
+
+let plansRef = db.collection("plans");
 /*
 DATA STRUCTURE
 
@@ -14,10 +29,9 @@ plans (collection)
                 availableTimes : {Sun : [8-10, 2-3], Sat: ...}
 */
 
-const createEvent = (id) => {
-    let hashids = new Hashids("event salt");
-    let urlHash = hashids.encode(id);
+const createEvent = id => {
+	let hashids = new Hashids("event salt");
+	let urlHash = hashids.encode(id);
 
-    
-    return urlHash;
-}
+	return urlHash;
+};
