4ac97f4304c80c269100e8998ebcd97e496a6762
Black <gmocore@gmail.com>
2019-08-22 13:55:27 -0600

    modified checkbox styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,27 @@
+const eventRedirect = async (name, email, title, week) => {
+  let urlHash = await createEvent(name, email, title, week);
+  window.location.replace(window.location.href + "?event=" + urlHash);
+};
+
+$("#submit").click(function() {
+  eventRedirect("Gerritt", "test@tester.com", "Big Party", "8/18/2019");
+  // when clicked, input values get pushed to firebase
+});
+
+let urlParams = new URLSearchParams(window.location.search);
+
+if (urlParams.has("event")) {
+  //this block only runs if the user is on a page that contains an event query string
+  roomHash = urlParams.get("event");
+} else {
+  const eventRedirect = async ({ name, email, title, week } = {}) => {
+    let urlHash = await createEvent(name, email, title, week);
+    window.location.replace(window.location.href + "?event=" + urlHash);
+  };
+
+  // example event call: eventRedirect({name: "Cody",email: "test@tester.com",title: "Big Party", week: "8/18/2019"})
+}
+
 $("document").ready(function() {
   $(".participant").hide();
   $(".date-selection").hide();
@@ -34,3 +58,19 @@ $("#time-submit").click(e => {
     }
   }
 });
+
+function popularTime() {
+  //pulled from stack overflow
+  /* var store = ["1", "2", "2", "3", "4"];
+  var frequency = {}; // array of frequency.
+  var max = 0; // holds the max frequency.
+  var result; // holds the max frequency element.
+  for (var v in store) {
+    frequency[store[v]] = (frequency[store[v]] || 0) + 1; // increment frequency.
+    if (frequency[store[v]] > max) {
+      // is this frequency > max so far ?
+      max = frequency[store[v]]; // update max.
+      result = store[v]; // update result.
+    }
+  } */
+}
