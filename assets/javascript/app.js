204bccc60743d876b3d8076d151298ec1e895aab
Guillermo Villalta <guillermovillalta@Guillermos-iMac.local>
2019-08-21 21:10:47 -0600

    added javascript to render new div containing textarea with url of active page + copy url to clipboard

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,3 +4,29 @@ $('#submit').click(function(){
     // when clicked, input values get pushed to firebase
 
 })
+
+$('#').click(function(){
+    showLink();
+});
+
+var showLink = () => {
+    let body = $(document.body)
+    let url = window.location.href;
+    
+    body.empty();
+    newDiv = $('<div>');
+    newTextArea = $('<textarea>');
+    newTextArea.text(url);
+    newTextArea.select();
+    newLegend = $('<h1>');
+    newLegend.text('Share this link with the people you want to invite');
+    newSubLegend = $('<h3>')
+    newSubLegend.text('Link has been copied to clipboard');
+    
+    newDiv.append(newTextArea);
+    newDiv.append(newLegend);
+    newDiv.append(newSubLegend);
+    body.append(newDiv);
+
+    document.execCommand("copy");
+}
